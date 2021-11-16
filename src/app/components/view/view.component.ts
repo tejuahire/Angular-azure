3013a2dc0800e25b027acd0f9e20cfdd8a816be6
TejaswiniAhire <tejaswini.ahire@atyeti.com>
2021-11-16 18:15:01 +0530

    home and view changes

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
 import { Router, ActivatedRoute } from '@angular/router';
 import { GetDataService } from '../../services/get-data.service';
 
@@ -35,6 +36,77 @@ export class ViewComponent implements OnInit {
   }
 
 
+ public lineChartData: ChartDataSets[] = [
+    { data: [380, 350, 370, 390, 360, 370, 400, 380, 390], label: 'company', yAxisID: 'y-axis-1' }
+  ];
+  public lineChartLabels: Label[] = ['9:15 AM', '10 AM', '11 AM', '11:30 AM', '12 PM', '1 PM', '2 PM', '3 PM', '3:30 PM'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{}],
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          position: 'left',
+          gridLines: {
+            color: 'rgba(255,0,0,0.3)',
+          },
+          ticks: {
+            fontColor: 'red',
+          }
+        }
+      ]
+    },
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno'
+          }
+        },
+      ],
+    },
+  };
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // red
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  // never remove this comment
+  // @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
+}
 
 
 
