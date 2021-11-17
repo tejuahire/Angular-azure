cfc3cb3c21081b86f59295271c831e6be7fc4796
Aditya Vanjari <aditya.vanjari@atyeti.com>
2021-11-17 15:57:13 +0530

    single login page for admin and user

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,13 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authenticate.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
-export class LoginComponent{
+export class LoginComponent {
 
     username = '';
     password = '';
@@ -17,22 +17,20 @@ export class LoginComponent{
     invalidUser = false;
     switchToLoginPage = true;
     errorMessage = '';
-    showImage=true;
+    showImage = true;
 
-  
-    constructor(private router: Router,private loginservice: AuthenticationService) {  }
 
-    checkValidLogin(mail,pass)
-    {
-        if(mail.errors || pass.errors)
+    constructor(private router: Router, private loginservice: AuthenticationService) { }
+
+    checkValidLogin(mail, pass) {
+        if (mail.errors || pass.errors)
             return true;
         else
             return false;
     }
 
-    checkValidUser(mail,user,ph,pass)
-    {
-        if(mail.errors || user.errors || ph.errors || pass.errors)
+    checkValidUser(mail, user, ph, pass) {
+        if (mail.errors || user.errors || ph.errors || pass.errors)
             return true;
         else
             return false;
@@ -40,65 +38,75 @@ export class LoginComponent{
 
 
 
-    checkLogin(mail,pass) 
-    {
-        if(mail.errors || pass.errors){
+    checkLogin(mail, pass) {
+
+        if (mail.errors || pass.errors) {
+            console.log(mail);
             return;
         }
-        
-        this.loginservice.authenticate(this.mailid, this.password) 
-        .subscribe( 
-            data  => { 
-                        if(data.status==='success')
-                        {          
+        if (this.mailid == "abc@atyeti.com") {
+            if (this.password == 'abc@123') {
+                this.router.navigate(['/admin']);
+            }
+            else {
+                console.log("invalid password");
+            }
+        }
+        else {
+            this.loginservice.authenticate(this.mailid, this.password)
+                .subscribe(
+                    data => {
+                        if (data.status === 'success') {
                             localStorage.setItem('username', this.mailid);
                             this.invalidLogin = false;
+
                             this.router.navigate([''])
                         }
-                        else
-                        {
+                        else {
                             this.errorMessage = "Invalid Credentials";
                             this.invalidLogin = true
                         }
                     },
-            error => { 
-                        if(error.error.message==="Incorrect credentials" || error.error.message==="User doesn't exist")
+                    error => {
+                        if (error.error.message === "Incorrect credentials" || error.error.message === "User doesn't exist")
                             this.errorMessage = error.error.message;
                         this.invalidLogin = true
                     }
-            );     
+                );
+        }
+
     }
 
-    registerNewUser()
-    { 
-        this.loginservice.addNewUser(this.mailid,this.username,this.phone,this.password)
-        .subscribe( 
-            data  => { 
-                        this.switchToLoginPage = true;
-                        this.password = '';
-                        this.invalidUser = false;
-                        this.invalidLogin = false;
-                        this.switchToLogin();
-                        },
-            error  => { console.log(error);
-                        if(error.error.message==="User already exists, please check email.")
-                            this.errorMessage = "email already exists";
-                        else if(error.error.message==="User already exists, please check phone number.")
-                            this.errorMessage = "Phone number already exists";
+    registerNewUser() {
+        this.loginservice.addNewUser(this.mailid, this.username, this.phone, this.password)
+            .subscribe(
+                data => {
+                    this.switchToLoginPage = true;
+                    this.password = '';
+                    this.invalidUser = false;
+                    this.invalidLogin = false;
+                    this.switchToLogin();
+                },
+                error => {
+                    console.log(error);
+                    if (error.error.message === "User already exists, please check email.")
+                        this.errorMessage = "email already exists";
+                    else if (error.error.message === "User already exists, please check phone number.")
+                        this.errorMessage = "Phone number already exists";
 
-                        this.invalidUser = true;
-                    }
+                    this.invalidUser = true;
+                }
             );
     }
 
-    switchToRegister(){
-    this.switchToLoginPage = false;
-    this.showImage=false;
+    switchToRegister() {
+        this.switchToLoginPage = false;
+        this.showImage = false;
     }
 
-    switchToLogin(){
-    this.switchToLoginPage = true;
-    this.showImage=true;
+    switchToLogin() {
+        this.switchToLoginPage = true;
+        this.showImage = true;
     }
 
 }
